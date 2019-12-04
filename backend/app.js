bd50eddbe65dcb6070ea3483482241d17a6477fe
Ivan Efimov <user@MacBook-Pro-User-2.local>
2019-12-05 01:55:32 +0300

    fix(cors): swagger works now

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const auth = require('./routes/auth')
 const bet = require('./routes/bet')
 const random01 = require('./routes/random01')
@@ -11,6 +12,7 @@ const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
